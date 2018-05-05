72783e1314b88448af2a0bacb93260e9b1411b07
xiang <coldgemini@foxmail.com>
2018-05-05 20:41:24 +0800

    setup links

--- configs/link-files/setuplinks.sh
+++ configs/link-files/setuplinks.sh
@@ -1,56 +1,73 @@
 #echo 'source ~/.bashrc.user' >> ~/.bashrc
-if [ ! -e $HOME/.backups ];then
+if [ ! -d $HOME/.backups ];then
+	echo "creating .backups folder"
 	mkdir $HOME/.backups
 fi
 
-if [ -e $HOME/.Xresources ];then
+if [ -L $HOME/.Xresources ];then
+	echo "moving .Xresources to .backups folder"
 	mv $HOME/.Xresources $HOME/.backups/.Xresources
 	ln -s $PWD/urxvt/Xresources.1 $HOME/.Xresources;
 else
 	ln -s $PWD/urxvt/Xresources.1 $HOME/.Xresources;
 fi
-if [ -e $HOME/.i3status.conf ];then
+if [ -L $HOME/.i3status.conf ];then
+	echo "moving .i3status.conf to .backups folder"
 	mv $HOME/.i3status.conf $HOME/.backups/.i3status.conf
 	ln -s $PWD/i3status.conf $HOME/.i3status.conf;
 else
 	ln -s $PWD/i3status.conf $HOME/.i3status.conf;
 fi
-if [ -e $HOME/.i3 ];then
+if [ -L $HOME/.i3 ];then
+	echo "moving .i3 folder to .backups folder"
 	mv $HOME/.i3 $HOME/.backups/.i3
 	ln -s $PWD/i3 $HOME/.i3;
 else
 	ln -s $PWD/i3 $HOME/.i3;
 fi
-if [ ! -e $HOME/.vimlocal ];then
+if [ ! -L $HOME/.vimlocal ];then
+	echo "moving .vimlocal folder to .backups folder"
 	mkdir -p $HOME/.vimlocal/plugged
 fi
-if [ -e $HOME/.vim ];then
+if [ -L $HOME/.vim ];then
+	echo "moving .vim folder to .backups folder"
 	mv $HOME/.vim $HOME/.backups/.vim
 	ln -s $PWD/vim $HOME/.vim;
 else
 	ln -s $PWD/vim $HOME/.vim;
 fi
-if [ -e $HOME/.vimrc ];then
+if [ -L $HOME/.vimrc ];then
+	echo "moving .vimrc to .backups folder"
 	mv $HOME/.vimrc $HOME/.backups/.vimrc
 	ln -s $PWD/vimrc $HOME/.vimrc;
 else
 	ln -s $PWD/vimrc $HOME/.vimrc;
 fi
-if [ -e $HOME/.pip ];then
+if [ -L $HOME/.pip ];then
+	echo "moving .pip to .backups folder"
 	mv $HOME/.pip $HOME/.backups/.pip
 	ln -s $PWD/pip $HOME/.pip;
 else
 	ln -s $PWD/pip $HOME/.pip;
 fi
-if [ -e $HOME/.bashrc.user ];then
+if [ -L $HOME/.bashrc.user ];then
+	echo "moving .bashrc.user to .backups folder"
 	mv $HOME/.bashrc.user $HOME/.backups/.bashrc.user
 	ln -s $PWD/bashrc.user $HOME/.bashrc.user;
 else
 	ln -s $PWD/bashrc.user $HOME/.bashrc.user;
 fi
-if [ -e $HOME/.wgetrc ];then
+if [ -L $HOME/.wgetrc ];then
+	echo "moving .wgetrc to .backups folder"
 	mv $HOME/.wgetrc $HOME/.backups/.wgetrc
 	ln -s $PWD/wgetrc $HOME/.wgetrc;
 else
 	ln -s $PWD/wgetrc $HOME/.wgetrc;
+fi
+if [ -L $HOME/.urxvt ];then
+	echo "moving .urxvt to .backups folder"
+	mv $HOME/.urxvt $HOME/.backups/.urxvt
+	ln -s $PWD/urxvt $HOME/.urxvt;
+else
+	ln -s $PWD/urxvt $HOME/.urxvt;
 fi
---
+++ configs/setup-scripts/language-setup.sh
@@ -0,0 +1,1 @@
+sudo apt-get install zh-autoconvert zhcon fonts-droid-fallback ttf-wqy-zenhei ttf-wqy-microhei fonts-arphic-ukai fonts-arphic-uming language-pack-zh-hans fonts-font-awesome dbus-x11 fcitx-table-wbpy
